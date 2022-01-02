7d37654abb9e82d555ea70e69e417a1f0a61243d
전은형 <ooooo5h91@gmail.com>
2022-01-02 19:29:11 +0900

    무한반복 => 0번 종료 처리

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import db_handler
 
 # 메인 메뉴 출력 기능
 def show_main_menu():
-    print('===== 강의 관리 시스템 (LMS) =====')
-    print('1. 수강생 목록 조회')
-    print('0. 프로그램 종료')
-    print('=================================')
-    num = int(input('메뉴 선택 : '))
-    
-    if num == 0:
-        print(' 프로그램을 종료합니다.')
-    elif num == 1:
-        get_user_list_from_db
+    while True:
+        print('===== 강의 관리 시스템 (LMS) =====')
+        print('1. 수강생 목록 조회')
+        print('0. 프로그램 종료')
+        print('=================================')
+        num = int(input('메뉴 선택 : '))
+        
+        if num == 0:
+            print(' 프로그램을 종료합니다.')
+            break
+        elif num == 1:
+            get_user_list_from_db
     
     
 # 1번 누르면 => DB에서 수강생 목록 조회 요청
